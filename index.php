94d3df3de4030a23b0acd3c2fadabf8c4a28413d
Lois Girard <lois26.girard@gmail.com>
2026-05-16 15:31:33 +0200

    ajout html pour accueillir le json

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ session_start();
 require_once 'config.php';
 $pdo = getPDO();
 
-
-
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit;
@@ -12,5 +10,34 @@ if (!isset($_SESSION['user_id'])) {
 $userId = $_SESSION['user_id'];
 ?>
 
-<a href="logout.php">Déconnexion</a>
-<p>Ca fonctionne !</p>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Mes tâches - Todo List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <main>
+        <a href="logout.php">Déconnexion</a>
+        <h1>Mes tâches</h1>
+
+        <!-- filtres + pagination -->
+        <!-- liste des tâches -->
+
+        <section id="tasks-section">
+            <ul id="tasks-list">
+                <!-- tâches en js -->
+            </ul>
+        </section>
+
+        <!-- message d'erreur (ou chargement) -->
+        <div id="tasks-message"></div>
+    </main>
+
+    <script src="app.js"></script>
+</body>
+</html>
