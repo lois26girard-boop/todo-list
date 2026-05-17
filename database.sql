c382f91b7000a157e2c5e5f7aa238d5f9934728e
Lois Girard <lois26.girard@gmail.com>
2026-05-17 23:43:22 +0200

    maj bdd sql pour dump

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2026 at 07:47 PM
+-- Generation Time: May 17, 2026 at 11:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -46,7 +46,23 @@ INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `due_date`, `prior
 (1, 1, 'Acheter du lait', 'Passer au supermarché en rentrant du travail', '2026-05-17', 'normal', 0, '2026-05-16 15:08:27'),
 (2, 1, 'Réviser PHP', 'Relire le code du TP todo et tester l’API', '2026-05-20', 'high', 0, '2026-05-16 15:08:27'),
 (4, 1, 'Payer la facture d’électricité', 'Vérifier le montant avant paiement', '2026-05-10', 'high', 0, '2026-05-16 15:08:27'),
-(5, 1, 'Appeler le dentiste', 'Prendre rendez-vous pour contrôle', '2026-05-25', 'normal', 0, '2026-05-16 15:08:27');
+(5, 1, 'Appeler le dentiste', 'Prendre rendez-vous pour contrôle', '2026-05-25', 'normal', 0, '2026-05-16 15:08:27'),
+(7, 2, 'test', NULL, NULL, 'normal', 0, '2026-05-17 23:13:35'),
+(8, 1, 'Répondre aux emails', 'Faire le tri dans la boîte de réception et répondre aux messages importants', '2026-05-26', 'normal', 0, '2026-05-17 20:00:00'),
+(9, 1, 'Planifier la semaine', 'Lister les tâches prioritaires pour la semaine prochaine', '2026-05-27', 'high', 0, '2026-05-17 20:05:00'),
+(10, 1, 'Faire une sauvegarde du code', 'Sauvegarder le projet Todo List sur Git ou sur une clé USB', '2026-05-28', 'normal', 0, '2026-05-17 20:10:00'),
+(11, 1, 'Lire un article sur PHP', 'Choisir un article sur les bonnes pratiques PDO', '2026-05-29', 'low', 0, '2026-05-17 20:15:00'),
+(12, 1, 'Tester les messages d’erreur', 'Vérifier les cas de login/inscription avec mauvais mot de passe', '2026-05-30', 'normal', 0, '2026-05-17 20:20:00'),
+(13, 1, 'Nettoyer la base de données', 'Supprimer les données de test inutiles dans gestion_todo', '2026-05-31', 'low', 0, '2026-05-17 20:25:00'),
+(14, 1, 'Ajouter des commentaires au code', 'Documenter les parties importantes dans index.php et login.php', '2026-06-01', 'normal', 0, '2026-05-17 20:30:00'),
+(15, 1, 'Tester la déconnexion', 'S’assurer que logout.php détruit bien la session', '2026-06-02', 'high', 0, '2026-05-17 20:35:00'),
+(16, 1, 'Vérifier la sécurité', 'Contrôler que les pages protégées redirigent bien vers login.php', '2026-06-03', 'high', 0, '2026-05-17 20:40:00'),
+(17, 1, 'Essayer un autre navigateur', 'Tester l’application sur Chrome, Firefox et Edge', '2026-06-04', 'low', 0, '2026-05-17 20:45:00'),
+(18, 1, 'Améliorer le CSS', 'Ajuster les marges et la mise en page des tâches', '2026-06-05', 'normal', 0, '2026-05-17 20:50:00'),
+(19, 1, 'Tester la pagination', 'Naviguer entre les pages 1, 2, 3 et vérifier le nombre de tâches', '2026-06-06', 'high', 0, '2026-05-17 20:55:00'),
+(20, 1, 'Essayer des tâches terminées', 'Marquer quelques tâches comme faites pour voir le style .task-done', '2026-06-07', 'normal', 0, '2026-05-17 21:00:00'),
+(21, 1, 'Créer une tâche en retard', 'Ajouter une tâche avec due_date dans le passé pour tester .task-late', '2026-05-10', 'high', 0, '2026-05-17 21:05:00'),
+(22, 1, 'Relire le code', 'Parcourir tous les fichiers PHP pour repérer les améliorations possibles', '2026-06-08', 'normal', 0, '2026-05-17 21:10:00');
 
 -- --------------------------------------------------------
 
@@ -66,7 +82,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password_hash`, `created_at`) VALUES
-(1, 'trucbox@gmail.com', '$2y$10$y.i5l3n1KpbwJ9N.gwetQuLssljzgvWNg1gGEIf9ooNskYbosNCdK', '2026-05-13 23:46:29');
+(1, 'trucbox@gmail.com', '$2y$10$y.i5l3n1KpbwJ9N.gwetQuLssljzgvWNg1gGEIf9ooNskYbosNCdK', '2026-05-13 23:46:29'),
+(2, 'compteultrasecret@gmail.com', '$2y$10$3QaKnIS27PzYL.Ydk01Tk.Oe5uCjd3T1K7uGi9cuA/HVhnmlGXSAO', '2026-05-17 23:12:50');
 
 --
 -- Indexes for dumped tables
@@ -94,13 +111,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
